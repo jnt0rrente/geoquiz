5eb5a7eb43923f7a953af362535828dfb862b73f
uo277963 <uo277963@uniovi.es>
2022-06-01 21:34:58 +0200

    avanzando

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,19 +1,39 @@
 "use strict"
 class UploadManager {
     constructor() {
-        this.acceptedType = /text.xml/
-        this
+        this.acceptedType = /text.xml/;
+        this.handler = new FormlParser();
     }
 
-    read() {
-        var file = document.getElementById("fileUpload").files[0];
+    read(files) {
+        var file = files[0];
 
-        if (file.type.match(this.acceptedType)) {
-            var fr = new FileReader();
-            fr.onload = this.onFileLoad;
-            fr.readAsText(file);
-        } else {
-            alert("tipo malo");
+        if (!file.type.match(this.acceptedType)) {
+            alert("mal tipo");
+            return;
         }
+
+        var reader = new FileReader();
+        reader.onload = function() {
+            try {
+                var fileContent = reader.result;
+                this.handler.handle(fileContent);
+            } catch (exception) {
+                console.log("Error: " + exception.message);
+            }
+        }.bind(this)
+
+        reader.readAsText(file);
     }
+
 }
+
+class FormlHandler {
+    constructor() {
+
+    }
+
+
+}
+
+var uploadManager = new UploadManager();
