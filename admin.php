ca59614e93148b755e56092909bb3bdb1ba291e3
uo277963 <uo277963@uniovi.es>
2022-06-03 22:10:52 +0200

    cambiados link de navbar a admin.php

--- admin.php
+++ admin.php
@@ -24,7 +24,7 @@
         <a href="history.html" accesskey="h" tabindex="2">Historical geology</a>
         <a href="mineralogy.html" accesskey="c" tabindex="3">Mineralogy</a>
         <a href="economic.html" accesskey="e" tabindex="4">Economic geology</a>
-        <a href="admin.html" accesskey="a" tabindex="5">Administration</a>
+        <a href="admin.php" accesskey="a" tabindex="5">Administration</a>
     </nav>
 
     <?php
@@ -34,7 +34,8 @@
                 <h3>Upload forml xml</h3>
                 <p>After defining a forml file, you can use this utility to upload it as a public quiz on this website.</p>
                 <input type='file' accept='text/xml' onchange='uploadManager.read(this.files)' />
-                <input type='button' name='uploadFile' value='Subir archivo' onclick='uploadManager.upload()'/> ";
+                <input type='button' name='uploadFile' value='Subir archivo' onclick='uploadManager.upload()'/> 
+        ";
 
     if (isset($_POST["inputFile"])) {
         $inputFile = $_POST["inputFile"];
