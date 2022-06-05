21d39d6db0086b160b663e598832f3fd92ca16e7
uo277963 <uo277963@uniovi.es>
2022-06-05 11:59:20 +0200

    metiendo el archivo jaj

--- scripts/admin.js
+++ scripts/admin.js
@@ -62,8 +62,9 @@ class FormlParser {
         quiz.description = parsedXML.getElementsByTagName("description")[0].nodeValue;
         quiz.questions = [];
 
-        var jsonQuestion = {};
+
         Array.from(parsedXML.getElementsByTagName("question")).forEach(question => {
+            var jsonQuestion = {};
             jsonQuestion.text = question.getAttribute("text");
             jsonQuestion.correct_option = question.getAttribute("accepted");
 
