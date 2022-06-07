aeda5b410997052c8ae335d5481eccee6027d664
uo277963 <uo277963@uniovi.es>
2022-06-07 16:32:27 +0200

    fix a upload

--- quiz.php
+++ quiz.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial scale=1.0" />
 
     <link rel="stylesheet" type="text/css" href="styles/style.css">
-    <link rel="stylesheet" type="text/css" href="styles/single_quiz.css">
+    <link rel="stylesheet" type="text/css" href="styles/quiz.css">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="scripts/quiz.js"></script>
