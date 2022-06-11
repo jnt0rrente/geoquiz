249ff49be0cf46d824650002c21eb244a06c1ba0
uo277963 <uo277963@uniovi.es>
2022-06-11 16:24:36 +0200

    options

---
+++ scripts/quiz_list.js
@@ -0,0 +1,27 @@
+class ReverseGeocoder {
+    constructor() {
+        this.apikey = "e1d215c28c8c49389af3cc37b829c5f5";
+        this.url = "https://api.opencagedata.com/geocode/v1/json";
+    }
+
+    getContinentForCoordinates(lat, long) {
+
+    }
+
+    buildUrl(lat, long) {
+        let returnUrl = this.url +
+            "?key=" + this.apikey +
+            "&q=" + encodeURIComponent(lat + "," + long);
+    }
+}
+
+class QuizListManager {
+
+    checkLocationRestriction() {
+
+    }
+
+}
+
+let qlm = new QuizListManager();
+window.onload(qlm.checkLocationRestriction());
